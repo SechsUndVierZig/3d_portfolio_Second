6f199f8991bf9a7c9c1c349c9376e114ab1e5aaf
SechsUndVierZig <IchigoNiclas1@gmail.com>
2023-12-17 13:16:55 +0200

    Pages update

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from "./Home.jsx";
+import Contact from "./Contact.jsx";
+import About from "./About.jsx";
+import Projects from "./Projects.jsx";
+
+export {Home,About,Contact,Projects}
